d7aa65190c43fcef6e0ed34bed44feed7c9855ff
mushBrainDave <dswanson@unomaha.edu>
2020-11-09 19:51:55 -0600

    remove unused code

--- src/router/index.js
+++ src/router/index.js
@@ -2,8 +2,6 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
 import Auth from '@/components/Auth'
-import CustomerList from '@/components/CustomerList'
-import CustomerCreate from '@/components/CustomerCreate'
 import SettingList from '@/components/SettingsList'
 import SettingCreate from '@/components/SettingsCreate'
 import IntakeList from '@/components/IntakeList'
@@ -19,26 +17,6 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Home
-    },
-    {
-      path: '/customer-list',
-      name: 'CustomerList',
-      component: CustomerList
-    },
-    {
-      path: '/customer-list/:msg',
-      name: 'CustomerUpdatedList',
-      component: CustomerList
-    },
-    {
-      path: '/customer-create',
-      name: 'CustomerCreate',
-      component: CustomerCreate
-    },
-    {
-      path: '/customer-create/:pk',
-      name: 'CustomerUpdate',
-      component: CustomerCreate
     },
     {
       path: '/settings-list',
