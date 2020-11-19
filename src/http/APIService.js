f955dbe15d8234e844147aba67c4d254645323b7
mushBrainDave <54049277+mushBrainDave@users.noreply.github.com>
2020-11-19 17:51:13 -0600

    Link to Heroku backend

--- src/http/APIService.js
+++ src/http/APIService.js
@@ -1,6 +1,6 @@
 /* eslint-disable */
 import axios from 'axios';
-const API_URL = 'http://127.0.0.1:8000'; /* http://groyce.pythonanywhere.com or http://localhost:8000 */
+const API_URL = 'https://calorie-c.herokuapp.com'; /* http://groyce.pythonanywhere.com or http://localhost:8000 */
 
 export class APIService {
 
